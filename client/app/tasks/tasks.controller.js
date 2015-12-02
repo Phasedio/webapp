2d609c527816e5ef2b87438c3e71e1173aba7138
Dave Riedstra <dave.riedstra@gmail.com>
2015-12-02 11:26:49 -0500

    basic logic for getting task data (name, category, assignee) from modal

--- client/app/tasks/tasks.controller.js
+++ client/app/tasks/tasks.controller.js
@@ -239,15 +239,80 @@ angular.module('webappApp')
           if(!thisSet){
             ref.child('profile-in-waiting').push({teams : { 0 : Auth.team},email : names.email, inviteEmail: $scope.currentUser.email, inviteName: $scope.currentUser.name });
             ref.child('profile-in-waiting2').push({teams : { 0 : Auth.team},email : names.email, inviteEmail: $scope.currentUser.email, inviteName: $scope.currentUser.name });
-
-
-            //sendTheMail(msg);
           }
         });
       }
     });
     $('#myModal').modal('toggle');
   };
+
+
+  $scope.addTask = function(newTask){
+    ga('send', 'event', 'task', 'task added');
+
+    // incoming object
+    console.log('newTask', newTask);
+
+    // check form errors
+    // if($scope.taskForm.$error.maxlength) {
+    //   alert('Your update is too long!');
+    //   return;
+    // }
+    
+
+    // format object to send to db
+    var taskPrefix = '';
+    var status = {
+      name: taskPrefix + newTask.name,
+      time: new Date().getTime(),
+      user: Auth.user.uid,
+      cat : newTask.category ? newTask.category : '',
+      city: $scope.city ? $scope.city : 0,
+      weather: weather,
+      taskPrefix : taskPrefix,
+      photo : $scope.bgPhoto ? $scope.bgPhoto : 0,
+      location: {
+        lat : $scope.lat ? $scope.lat : 0,
+        long : $scope.long ? $scope.long : 0
+      },
+      assignee : newTask.assignee.uid
+    };
+
+    // babbys first status
+    console.log('status', status);
+
+    return; // tmp
+
+    // push update to db
+    var teamRef = new Firebase(FURL),
+      team = $scope.team.name;
+    teamRef.child('team').child(team).child('task').child(Auth.user.uid).set(status);
+    teamRef.child('team').child(team).child('all').child(Auth.user.uid).push(status,function(){
+      console.log('status set');
+      $scope.updateStatus = '';
+      //we are getting the user.uid, we need to extract the member off the user.uid.
+      //then we can do a scope.setSelected off that member.
+
+        //Send push notifications to team
+      // $http.get('http://45.55.200.34:8080/push/update/'+team+'/'+Auth.user.name+'/'+status.name,'').success(function(data){
+      //   //alert(data);
+      // });
+    });
+
+
+    //reset scope
+    $scope.task = update;
+    $scope.task.name = '';
+
+    $scope.showTaskView = true;
+    $scope.taskTime = status.time; // we didnt have status.time so i think this fixes the problem(?)
+    // maybe we need a timeout function here to run around out $apply()??
+
+    //$scope.$apply();
+
+    //need to find out what the member/who is
+    //$scope.getTaskHistory(member);
+  }
 
   $scope.addTaskModal = function(){
     ga('send', 'event', 'Modal', 'Member add');
