633b5f5168895539a07832371e10326484c26473
Dave Riedstra <dave.riedstra@gmail.com>
2015-12-10 14:13:39 -0500

    wired up and debugged activeStream and activeFilter

--- client/app/tasks/tasks.controller.js
+++ client/app/tasks/tasks.controller.js
@@ -64,6 +64,8 @@ angular.module('webappApp')
     $scope.viewType = Phased.viewType;
     $scope.taskPriorities = Phased.TASK_PRIORITIES; // in new task modal
     $scope.taskStatuses = Phased.TASK_STATUSES; // in new task modal
+    $scope.taskPriorityID = Phased.TASK_PRIORITY_ID;
+    $scope.taskStatusID = Phased.TASK_STATUS_ID;
     $scope.myID = Auth.user.uid;
 
     $scope.today = new Date().getTime(); // min date for deadline datepicker
@@ -71,6 +73,9 @@ angular.module('webappApp')
     $scope.assignments = Phased.assignments;
     $scope.archive = Phased.archive;
     $scope.showArchive = false;
+
+    $scope.activeStream = $scope.assignments.to_me;
+    $scope.activeFilter = '!1'; // not completed tasks
 
     $scope.sortable = [
       'cat', 'deadline', 'priority', 'name', 'date', 'assigned_by'
@@ -90,6 +95,67 @@ angular.module('webappApp')
     **  event handlers
     **
     */
+
+    // validates stream and filter, then sets active task stream
+    // optionally gets archive if not present
+    $scope.setActiveStream = function(streamName, filter) {
+      console.log('setting active stream', streamName, filter);
+      // check and set status
+      switch (streamName) {
+        case 'assignments.all':
+          $scope.activeStream = $scope.assignments.all;
+          break;
+        case 'assignments.to_me':
+          $scope.activeStream = $scope.assignments.to_me;
+          break;
+        case 'assignments.by_me':
+          $scope.activeStream = $scope.assignments.by_me;
+          break;
+        case 'assignments.unassigned':
+          $scope.activeStream = $scope.assignments.unassigned;
+          break;
+        case 'archive.to_me':
+          if (!('to_me' in $scope.archive)) Phased.getArchiveFor('to_me');
+          $scope.activeStream = $scope.archive.to_me;
+          console.log('arch', $scope.archive);
+          break;
+        case 'archive.all':
+          Phased.getArchiveFor('all');
+          $scope.activeStream = $scope.archive.all;
+          console.log('arch', $scope.archive);
+          break;
+        default:
+          console.log('switch default');
+          $scope.activeStream = $scope.assignments.to_me;
+          filter = '!' + Phased.TASK_STATUS_ID.COMPLETE;
+          break;
+      }
+
+      // check and set filter
+      if (!filter || typeof filter == 'undefined') {
+        $scope.activeFilter = undefined;
+      } else {
+        filter = filter.toString();
+
+        // check for negating character
+        var filterNot = false;
+        if (filter[0] === '!') {
+          filterNot = true;
+          filter.slice(1);
+        }
+
+        // default to 'not COMPLETE' on false
+        if (!(filter in Phased.TASK_STATUSES)) {
+          filter = '1';
+          filterNot = true;
+        }
+
+        // set activeFilter
+        $scope.activeFilter = filterNot ? '!' + filter : filter;
+      }
+
+      console.log('active stream set:', $scope.activeStream, $scope.activeFilter);
+    }
 
     $scope.addTask = function(newTask){
       // incoming object
