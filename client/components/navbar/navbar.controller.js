7345949cf2ff15d36d92060dbcaa91e0abe0e37a
Dave Riedstra <dave.riedstra@gmail.com>
2016-01-13 09:55:39 -0800

    show admin if profile data not updated

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -23,13 +23,17 @@ angular.module('webappApp')
     }
     ];
 
-    $scope.$on('Phased:currentUserProfile', function(){
+    // add Admin link if user has permissions
+    var addAdminNavOption = function(){
       if (Auth.user.role == 'Admin' || Auth.user.role == 'Owner') 
         $scope.menu.push({ 'title': 'Admin', 'link' : '/admin' });
       else
         if ($scope.menu[$scope.menu.length - 1].title == 'Admin')
           $scope.menu.pop();
-    });
+    }
+
+    addAdminNavOption(); // in case of moving within app and not updating profile
+    $scope.$on('Phased:currentUserProfile', addAdminNavOption);
 
     $scope.isCollapsed = true;
 
