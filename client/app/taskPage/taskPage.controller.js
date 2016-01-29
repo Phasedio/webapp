248c1d3b8358d6ee9e359e728fd5ab6aef445936
Brian Best <brian.brianb64@gmail.com>
2016-01-29 11:15:26 -0400

    Task pages will now wait for phased provider to be set up before kicking users out

--- client/app/taskPage/taskPage.controller.js
+++ client/app/taskPage/taskPage.controller.js
@@ -15,15 +15,37 @@ angular.module('webappApp')
     function init(){
       // get route peram and try to assign it
       console.log($scope.assignments);
+      //check if phased is set up
+      if(Phased.assigments){
+        if($routeParams.taskID && Phased.assignments.all[$routeParams.taskID]){
+          $scope.taskInfo = Phased.assignments.all[$routeParams.taskID];
+          console.log($scope.taskInfo);
+
+        }else{
+          //fail - send user to tasks page
+          alert("failed")
+          //$location.path("/tasks")
+        }
+      }
+
+    }
+
+
+    $scope.$on('Phased:setup', function() {
+
       if($routeParams.taskID && Phased.assignments.all[$routeParams.taskID]){
         $scope.taskInfo = Phased.assignments.all[$routeParams.taskID];
         console.log($scope.taskInfo);
 
       }else{
         //fail - send user to tasks page
+        alert("failed")
         //$location.path("/tasks")
       }
-    }
+
+      $scope.$apply();
+    });
+
     init();
 
 
