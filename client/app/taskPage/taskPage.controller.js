dc3f49b6032ba45cca338ed795f2d26d5f393b4d
Brian Best <brian.brianb64@gmail.com>
2016-01-29 11:46:51 -0400

    Fixed bug where user double clicks from tasks and nothing loads

--- client/app/taskPage/taskPage.controller.js
+++ client/app/taskPage/taskPage.controller.js
@@ -38,14 +38,14 @@ angular.module('webappApp')
       // get route peram and try to assign it
       console.log($scope.assignments);
       //check if phased is set up
-      if(Phased.assigments){
+      if(Phased.assignments){
         if($routeParams.taskID && Phased.assignments.all[$routeParams.taskID]){
           $scope.taskInfo = Phased.assignments.all[$routeParams.taskID];
           console.log($scope.taskInfo);
 
         }else{
           //fail - send user to tasks page
-          alert("failed")
+          //alert("failed")
           //$location.path("/tasks")
         }
       }
