4a2208dded1b18c8a3ef1f765066b75be2218bae
Brian Best <brian.brianb64@gmail.com>
2016-02-01 11:36:39 -0400

    Toggle member is having problems with older fb users with simplelogin:x

--- client/app/admin-csvDwl/admin-csvDwl.controller.js
+++ client/app/admin-csvDwl/admin-csvDwl.controller.js
@@ -9,11 +9,46 @@ angular.module('webappApp')
     $scope.team = Phased.team;
     $scope.Phased = Phased;
 
+
+    //form vars
+
+    var users = [],category = [],time = [];
+
     // bounce users without Admin or Owner permissions
     $scope.$on('Phased:currentUserProfile', function(){
       if (Auth.user.role == 'member')
         $location.path('/feed');
     });
+
+    $scope.toggleMember = function(uid){
+      var id = uid;
+      //check if uid is in arr
+      var x = _.findIndex(users, function(o) { return o == uid;});
+      console.log(x);
+      if(x != -1){
+        //if so remove it from arr
+        _.remove(users,function(o) { return o == uid;});
+        //remove active class to element
+        $("#"+uid).toggleClass("active");
+        //document.querySelectorAll("[data-member='"+uid+"']").className = "";
+
+      }else{
+        //if not add it
+        users.push(uid);
+        //add active class to element
+        $("#"+uid).toggleClass("active");
+        //document.querySelectorAll("[data-member='"+uid+"']").className = "active";
+
+      }
+      console.log(users);
+
+
+      //if not add it
+      //add active class to element
+
+    }
+
+
 
 
 
