64b336c38dee536451b48b031d18d86bc340682e
Brian Best <brian.brianb64@gmail.com>
2016-02-02 12:14:50 -0400

    system prints the right csv to console

--- server/api/download/download.controller.js
+++ server/api/download/download.controller.js
@@ -4,6 +4,7 @@ var _ = require('lodash');
 var fs = require('fs');
 var json2csv = require('json2csv');
 
+
 // Get list of downloads
 exports.index = function(req, res) {
   res.json([]);
@@ -13,37 +14,55 @@ exports.index = function(req, res) {
 exports.send = function(req, res) {
   console.log('got it');
   var dataJson = req.body.hose;
-  var dataKeys = Object.keys(dataJson);
-  console.log(dataJson);
+  var users = Object.keys(dataJson.data);
+  // get users
+  //console.log(dataJson);
   res.send(dataJson);
   //console.log(dataJson);
- //  var dataCsv = [];
- //  var fields = ['task', 'time', 'date'];
- //  for(var i = 0; i < dataKeys.length; i++){
- //  	console.log('new task');
- //  	var task = dataJson[dataKeys[i]];
- //  	var obj = {}
- //  	obj.time = new Date(task.time).toLocaleTimeString();
- //  	obj.date = new Date(task.time).toLocaleDateString();
- //  	obj.task = task.name;
- //  	dataCsv.push(obj);
- //  	console.log(dataCsv);
- //
- //  }
- //  console.log(dataCsv);
- //  json2csv({ data: dataCsv, fields: fields }, function(err, csv) {
- // if (err) console.log(err);
- // fs.writeFile('file.csv', csv, function(err) {
- //    if (err) throw err;
- // //    console.log('file saved');
- // //    res.set('Content-Type', 'application/octet-stream');
- // // res.set('Content-Disposition', 'attachment;filename=\"file.csv\"');
- // // res.download('file.csv');
- // res.send('file.csv');
-  	//});
+  var dataCsv = [];
+  var theCSV = '';
+  var fields = ['task','category', 'time', 'date'];
+  //Go through all users given to you
+  for (var y = 0; y < users.length; y++) {
+    //console.log();
+    // Figure out the names of all the keys
+    dataCsv = [];
+    if(dataJson.data[users[y]]){
+      var keys = Object.keys(dataJson.data[users[y]]);
+      console.log('hit that');
+      for(var i = 0; i < keys.length; i++){
+      	//console.log('new task');
+      	var task = dataJson.data[users[y]][keys[i]];
+      	var obj = {}
+      	obj.time = new Date(task.time).toLocaleTimeString();
+      	obj.date = new Date(task.time).toLocaleDateString();
+      	obj.task = task.name;
+      	dataCsv.push(obj);
+      	//console.log(dataCsv);
 
-		  // console.log(csv);
-	//});
+      }
+      json2csv({ data: dataCsv, fields: fields }, function(err, csv) {
+         if (err) console.log(err);
+
+         theCSV = theCSV +"\n"+ dataJson.dict[users[y]].name + ", , ,\n"+csv;
+        //  fs.writeFile('file.csv', csv, function(err) {
+        //     if (err) throw err;
+        //  //    console.log('file saved');
+        //  //    res.set('Content-Type', 'application/octet-stream');
+        //  // res.set('Content-Disposition', 'attachment;filename=\"file.csv\"');
+        //  // res.download('file.csv');
+        //  res.send('file.csv');
+      	//});
+
+    		  //console.log(csv);
+    	});
+      //console.log(dataCsv);
+    }
+
+
+  }
+  console.log(theCSV);
+  //res.send(theCSV);
 
 
 
