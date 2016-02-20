99194c5897b15e9ea071d55a204d4b8644c55c40
Brian Best <brian.brianb64@gmail.com>
2016-02-20 19:29:13 -0400

    Big style changes plus mix panel

--- client/components/sidebar/sidebar.controller.js
+++ client/components/sidebar/sidebar.controller.js
@@ -2,6 +2,7 @@
 
 angular.module('webappApp')
   .controller('SidebarCtrl', function ($scope, $location, Auth, Phased, FURL) {
+    $scope.showAdmin = false;
     $scope.menu = [
     {
       'title': 'Feed',
@@ -33,5 +34,20 @@ angular.module('webappApp')
     $scope.isActive = function(route) {
       return route === $location.path();
     };
+
+    var showAdminLink = function(){
+      // do only after Phased is set up
+      if (!Phased.SET_UP) {
+        $scope.$on('Phased:setup', showAdminLink);
+        return;
+      }
+
+      var myRole = Phased.team.members[Auth.user.uid].role;
+      if (myRole == Phased.ROLE_ID.ADMIN || myRole == Phased.ROLE_ID.OWNER)
+        $scope.showAdmin = true;
+      else
+        $scope.showAdmin = false;
+    }
+    showAdminLink(); // in case of moving within app and not updating profile
 
   });
