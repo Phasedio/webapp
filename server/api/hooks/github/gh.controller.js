6d1aab119b0788bf64254101a9bf274241029da2
Dave Riedstra <dave.riedstra@gmail.com>
2016-03-09 13:59:20 -0800

    webhook endpoint authenticates before FB requests (derp)

--- server/api/hooks/github/gh.controller.js
+++ server/api/hooks/github/gh.controller.js
@@ -4,11 +4,11 @@
 // copied from pushserver
 var Firebase = require("firebase");
 var FirebaseTokenGenerator = require("firebase-token-generator");
-var https = require('https');
 
 var FBRef = new Firebase("https://phaseddev.firebaseio.com/");
 var tokenGenerator = new FirebaseTokenGenerator("0ezGAN4NOlR9NxVR5p2P1SQvSN4c4hUStlxdnohh");
-var token = tokenGenerator.createToken({ uid: "gh-server"});
+var token = tokenGenerator.createToken({ uid: "hook-server"});
+
 var GHClientID = '84542af1ca986f17bd26';
 var GHClientSecret = '8f6d49d7be3e358ec229c97967055ce9551e122d';
 var auth1RedirectURI = 'http://localhost:9000/api/gh/auth2';
@@ -37,53 +37,70 @@ exports.index = function(req, res) {
 *
 */
 exports.repoPush = function(req, res) {
-	var teamID = req.params.team;
-	var pushEvent = req.data;
-
-	// 0. try to get team
-	FBRef.child('teams/' + teamID).once('value', function(snap) {
-		var team = snap.val();
-		console.log(team);
-
-		// 1. a) b) & c)
-		if (
-			!team // team doesn't exist
-			|| !( pushEvent.repository.id in team.repos ) // repo not registered to team
-			|| !('push' in team.repos[pushEvent.repository.id].hooks) // 'push' hook not registered on repo for team
-			) {
-			console.log(pushevent.repository.name + ' not registered, end');
+	// do after authenticated
+	FBRef.authWithCustomToken(token, function(error, authData) {
+		// fail if error
+		if (error) {
+			console.log(error);
+			res.end();
 			return;
 		}
 
-		// 2. check all members
-		var thePusher = false; // user ID
-		for (var i in team.members) {
-			if ('aliases' in team.members[i] && 'github' in team.members[i].aliases) {
-				for (var j in team.members[i].aliases.github) {
-					if (team.members[i].aliases.github[j] == pushEvent.head_commit.committer.username) {
-						thePusher = i;
-						break; // for j
-						break; // for i
+		var teamID = req.params.team;
+		var pushEvent = req.data;
+		console.log('repopush', teamID);
+
+		// 0. try to get team
+		FBRef.child('team/' + teamID).once('value', function(snap) {
+			var team = snap.val();
+			console.log('team data:', team);
+
+			// 1. a) b) & c)
+			if (
+				!team // team doesn't exist
+				|| !( pushEvent.repository.id in team.repos ) // repo not registered to team
+				|| !('push' in team.repos[pushEvent.repository.id].hooks) // 'push' hook not registered on repo for team
+				) {
+				var name = pushEvent && 'repository' in pushEvent ? pushEvent.repository.name : 'repository';
+				console.log(name + ' not registered, end');
+				res.end();
+				return;
+			}
+
+			// 2. check all members
+			var thePusher = false; // user ID
+			for (var i in team.members) {
+				if ('aliases' in team.members[i] && 'github' in team.members[i].aliases) {
+					for (var j in team.members[i].aliases.github) {
+						if (team.members[i].aliases.github[j] == pushEvent.head_commit.committer.username) {
+							thePusher = i;
+							break; // for j
+							break; // for i
+						}
 					}
 				}
 			}
-		}
 
-		// 3.
-		var statusText = pushEvent.pusher.name + ' pushed ' + pushEvent.size + ' commits to ' + pushEvent.repository.name;
-		var newStatus = {
-			name: statusText,
-			time: pushEvent.pushed_at,
-			user: thePusher // ID or false
-		};
+			// 3.
+			var statusText = pushEvent.pusher.name + ' pushed ' + pushEvent.size + ' commits to ' + pushEvent.repository.name;
+			var newStatus = {
+				name: statusText,
+				time: pushEvent.pushed_at,
+				user: thePusher // ID or false
+			};
 
-		console.log('updating status: ' + statusText);
-		FBRef.child('team/' + team + '/statuses').push(newStatus);
-		if (thePusher)
-			FBRef.child('team/' + team + '/members/' + thePusher + '/currentStatus').set(newStatus);
-		console.log('status updated');
+			console.log('updating status: ' + statusText);
+			FBRef.child('team/' + team + '/statuses').push(newStatus);
+			if (thePusher)
+				FBRef.child('team/' + team + '/members/' + thePusher + '/currentStatus').set(newStatus);
+			console.log('status updated');
 
-		// 4. issue notification — TODO
+			// 4. issue notification — TODO
 
+			// END
+			res.end();
+		}, function(err) {
+			console.log(err);
+		});
 	});
 }
