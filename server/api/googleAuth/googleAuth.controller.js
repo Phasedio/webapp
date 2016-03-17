67d8133ea967fe3f6bfdb26cda479c27bd68d348
Dave Riedstra <dave.riedstra@gmail.com>
2016-03-17 11:30:37 -0700

    GET /api/google/cal gets google calendars for a previously authed user (in session or in DB). setGOA2Creds() gets credentials from best source and provides an authorized client.

--- server/api/googleAuth/googleAuth.controller.js
+++ server/api/googleAuth/googleAuth.controller.js
@@ -3,9 +3,10 @@
 */
 
 'use strict';
-// gen vars
+// general vars
 // ====
 var config = require('../../config/environment');
+var Promise = require("promise");
 
 // Firebase vars
 // ====
@@ -14,7 +15,7 @@ var FirebaseTokenGenerator = require("firebase-token-generator");
 
 var FBRef = new Firebase("https://phaseddev.firebaseio.com/");
 var tokenGenerator = new FirebaseTokenGenerator(config.FB_SECRET_1);
-var token = tokenGenerator.createToken({ uid: "gcal-server"});
+var FBToken = tokenGenerator.createToken({ uid: "gcal-server"});
 
 
 // Google vars
@@ -26,8 +27,12 @@ var CLIENT_ID = '313573711545-p9bo68ve6d5oih51datnkv1i8vrumipq.apps.googleuserco
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 
+// this global oauth2Client can be used for getting auth tokens
+// but a new one should be used for making requests (as it will hold
+// a user's tokens internally and so shouldn't be global)
 var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 var scopes = ['https://www.googleapis.com/auth/calendar.readonly'];
+var GCal = google.calendar('v3');
 
 
 // routes
@@ -73,7 +78,7 @@ exports.auth2 = function(req, res) {
 			req.session.tokens = tokens;
 			saveUserTokens(req.session.user, tokens);
 		} else {
-			console.log('error:', err);
+			console.log(err);
 		}
 
 		// send respose after tokens saved to session
@@ -82,7 +87,38 @@ exports.auth2 = function(req, res) {
 		else
 			res.status(200).end();
 	});
+}
 
+/**
+*
+*	GET /api/google/cal
+*
+*	1. check Google auth
+* 2. get calendars from google endpoint
+*		using google oauth2Client
+*	3. return in same format to client
+*
+*/
+exports.getCals = function(req, res) {
+	// 1. a.
+	setGOA2Creds(req).then(
+		function success(authClient) {
+			// 2.
+			GCal.calendarList.list({
+				auth: authClient
+			}, function(err, response) {
+				if (response) {
+					res.status(200).send(response);
+				} else {
+					console.log(err);
+					res.status(err.code).send(err);
+				}
+			});
+		},
+		function failure() {
+			res.status(401).send('Unauthorized').end();
+		}
+	);
 }
 
 // internal business functions
@@ -112,7 +148,7 @@ var saveUserTokens = function(user, tokens) {
 	}
 	// 1.
 	if (user.provider !== 'password') {
-		FBRef.authWithCustomToken(token, function(error, authData) {
+		FBRef.authWithCustomToken(FBToken, function(error) {
 			if (error) {
 				console.log(error);
 				return;
@@ -130,7 +166,7 @@ var saveUserTokens = function(user, tokens) {
 			});
 		});
 	} else {
-		FBRef.authWithCustomToken(token, function(error, authData) {
+		FBRef.authWithCustomToken(FBToken, function(error) {
 			if (error) {
 				console.log(error);
 				return;
@@ -144,4 +180,44 @@ var saveUserTokens = function(user, tokens) {
 		FBRef.child('integrations/google/tokens/' + uid).update(tokens);
 		console.log('tokens updated');
 	}
+}
+
+/**
+*
+* Sets the google oauth2 client credentials
+*
+*	1. if we have session creds, use those
+*	2. otherwise get from DB
+*	3. otherwise reject promise
+*	
+*	returns a promise (due to async FB call)
+*	passes the fulfill method an authorized oauth2Client
+*	(that isn't in the global scope) to make this request
+*
+*/
+var setGOA2Creds = function(req) {
+	return new Promise(function(fulfill, reject) {
+		var _oa2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+		// 1.
+		if (req.session && 'tokens' in req.session && 'refresh_token' in req.session) {
+			_oa2Client.setCredentials(req.session.tokens); // user session tokens to auth client
+			fulfill(_oa2Client); // give callback authed client
+			return;
+		} else {
+			console.log('user tokens not in session, retrieving from DB');
+			FBRef.authWithCustomToken(FBToken, function(error) {
+				FBRef.child('integrations/google/tokens/' + req.session.user.uid).once('value', function (snap) {
+					var tokens = snap.val();
+					if (tokens) {
+						req.session.tokens = tokens; // save DB tokens to session
+						_oa2Client.setCredentials(tokens); // authorize client
+						fulfill(_oa2Client); // give callback authed client
+					} else {
+						console.log('no tokens in DB, failing');
+						reject();
+					}
+				});
+			});
+		}
+	});
 }
--- server/api/googleAuth/index.js
+++ server/api/googleAuth/index.js
@@ -7,5 +7,6 @@ var controller = require('./googleAuth.controller');
 router.get('/', controller.index);
 router.get('/auth1', controller.auth1);
 router.get('/auth2', controller.auth2);
+router.get('/cal', controller.getCals);
 
 module.exports = router;
