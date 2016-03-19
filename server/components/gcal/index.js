ceb2c4ad7fbb67a1d2b7aa603f29de0467d7c8d2
Dave Riedstra <dave.riedstra@gmail.com>
2016-03-18 18:45:24 -0700

    use calendar FBKey to stash in eventJobList; better documentation; dev schedule and logs

--- server/components/gcal/index.js
+++ server/components/gcal/index.js
@@ -30,7 +30,15 @@ var CLIENT_ID = config.google.CLIENT_ID,
 // internal business
 // ====
 var masterJob, // set in init
-	eventJobList = {}, // organized by calendar for cancelling later (ie, eventJobList[calID] = [job, job, ...])
+ 	// list of all scheduled jobs, indexed by calendar's FireBase key, which is
+ 	// guaranteed to be unique (important, since the same cal could be registered
+ 	// for multiple people or multiple teams)
+ 	// also our reference of which calendars do not need to be re-registered
+ 	// eventJobList = {
+	//		calFBKey : [job, job, ...], ...
+ 	// }
+	eventJobList = {},
+	
 	// time at which statuses should be posted for all day events. IN GMT!!! 24h, HH:MM:SS
 	// maybe a better solution to this. (ie, get timezone from calendar)
 	DAY_START_TIME = '12:00:00'; // 8AM EST
@@ -45,50 +53,63 @@ module.exports = function init() {
 		minute : 0 // leaving this null would run the job every minute!
 	};
 
+	// run the master job every 5 minutes to test
+	if (config.env === 'development')
+		rule = '*/5 * * * *';
+
 	masterJob = schedule.scheduleJob(rule, doMasterJob);
 	masterJob.job(); // invoke job immediately as well as when scheduled
+
+	schedule.scheduleJob('*/5 * * * *', function(){
+		console.log('scheduled jobs:', eventJobList);
+	});
 };
 
 /*
-	- Authenticate with firebase
-	- FB.on to monitor calendars
-		- whenever calendar added (incl when initiating),
-			do onCalAdd
-		- whenever calendar removed, do onCalRemoved
-	- remove last FB.on handler
-	- stash this handler for next iteration to remove
+	- 1. Authenticate with firebase
+	- 2. Remove last round's .on()
+	- 3. FB.on to monitor calendars:
+		- onUserAdd/Removed registers handlers for cals added/removed (also takes care of nesting by teams)
 */
 var doMasterJob = function() {
 	console.log('executing masterJob; next iteration at ', masterJob.pendingInvocations()[0].fireDate.toString());
-	// do after authenticated
+	// 1. do after authenticated
 	FBRef.authWithCustomToken(FBToken, function(error, authData) {
 		// fail if error
 		if (error)
 			return console.error('GCal schedule not running this time round due to Firebase auth error, will try again next time.', error);
+		
+		// 2.
+		FBRef.child('integrations/google/calendars').off(); // kills all event handlers
 
+		// 3. get this party started
 		FBRef.child('integrations/google/calendars').on('child_added', onUserAdd, console.log);
 		FBRef.child('integrations/google/calendars').on('child_removed', onUserRemoved, console.log);
 	});
 }
 
-/*
-	1. get Google credentials for user
-	2. get all events for each calendar on each team before next main job invocation
-	3. schedule doEventJob for each event
+/**
+*
+* registers all events for all cals for all teams on this user
+*
+*	1. immediately register all present calendars
+*	2. listen for changes and register all newly added calendars
+*			(using child_changed instead of child_added because it covers
+*			new calendars on known teams as well as new teams)	
+*
 */
 var onUserAdd = function(snap) {
-	console.log('onUserAdd', snap.val());
 	var userID = snap.key(),
 		calsByTeam = snap.val();
 
-	// set up initial scheduling
+	// 1. set up initial scheduling
 	setGOA2Creds(userID).then(function success(_oa2Client) {
 		for (var teamID in calsByTeam) {
 			registerCalsForTeam(_oa2Client, calsByTeam[teamID], userID, teamID);
 		}
 	});
 
-	// future-proofing: whenever the user's calendar registration changes
+	// 2. future-proofing: whenever the user's calendar registration changes
 	FBRef.child('integrations/google/calendars/' + userID).on('child_changed', function onTeamChanged(snap) {
 		var teamID = snap.key(),
 			cals = snap.val();
@@ -97,44 +118,63 @@ var onUserAdd = function(snap) {
 			registerCalsForTeam(_oa2Client, cals, userID, teamID);
 		});
 	});
-
-	/*
-		only register calendars that aren't already registered
-	*/
-	var registerCalsForTeam = function(_oa2Client, cals, userID, teamID) {
-		for (var i in cals) {
-			if (!(cals[i].id in eventJobList)) {
-				getAndRegisterEventsForCalendar(_oa2Client, cals[i], userID, teamID);
-
-				// watch newly registered cal for deregistration
-				FBRef.child('integrations/google/calendars/' + userID + '/' + teamID).on('child_removed', function teamChildRemoved(snap){
-					onCalRemoved(snap.val().id);
-					FBRef.child('integrations/google/calendars/' + userID + '/' + teamID).off(); // stop watching for deregistration
-				});
-			}
-		}
-	}
 }
 
-// deregister all events for all cals for all teams on this user
+/**
+*
+* deregister all events for all cals for all teams on this user
+*
+*/
 var onUserRemoved = function(snap) {
-	var userID = snap.key();
-	console.log(userID + ' rm', snap.val());
-	FBRef.child('integrations/google/calendars/' + userID).off(); // stop listening for this user's data
+	var userID = snap.key(),
+		calsByTeam = snap.val();
 
-	var teams = snap.val();
-	for (var teamID in teams) {
-		for (var i in teams[teamID])
-			onCalRemoved(teams[teamID][i].id);
+	// stop listening for this user's data
+	FBRef.child('integrations/google/calendars/' + userID).off();
+
+	// deregister all calendars
+	for (var teamID in calsByTeam)
+		for (var calFBKey in calsByTeam[teamID])
+			onCalRemoved(calFBKey);
+}
+
+/**
+*
+*	register all new calendars for the team
+*
+*	- checks if each calendar is registered
+*	- if it's not, 
+*		1. schedules posts for all of its events
+*		2. then watches for deregistration
+*
+*	NB: expects cals param to be indexed by Firebase key!
+*
+*/
+var registerCalsForTeam = function(_oa2Client, cals, userID, teamID) {
+	for (var calFBKey in cals) {
+		if (!(calFBKey in eventJobList)) {
+			// 1. schedule jobs for calendar events
+			onCalAdded(_oa2Client, cals[calFBKey], calFBKey, userID, teamID);
+		}
 	}
+
+	// 2. watch team for calendar deregistration
+	var address = 'integrations/google/calendars/' + userID + '/' + teamID;
+	FBRef.child(address).off(); // first get rid of any old handlers, since this fn will be called many times
+	FBRef.child(address).on('child_removed', function teamChildRemoved(snap){
+		onCalRemoved(snap.key());
+	});
 }
 
 /**
 *
 *	gets events from Google API and schedules jobs for them
 *
+*	- only gets events before the next master job cycle
+*	- ensures	that the calendar FBKey is listed in eventJobList
+*		even if there are no events this cycle
 */
-var getAndRegisterEventsForCalendar = function(_oa2Client, cal, userID, teamID) {
+var onCalAdded = function(_oa2Client, cal, calFBKey, userID, teamID) {
 	// ...get all events
 	var nextInvocation = masterJob.pendingInvocations()[0].fireDate.toISOString();
 	var params = {
@@ -146,7 +186,7 @@ var getAndRegisterEventsForCalendar = function(_oa2Client, cal, userID, teamID) 
 	};
 
 	// ensure cal evt job list exists (since list is also used as cal registration indicator)
-	eventJobList[cal.id] = eventJobList[cal.id] || {};
+	eventJobList[calFBKey] = eventJobList[calFBKey] || {};
 
 	// make API request
 	GCal.events.list(params, function(err, res) {
@@ -160,41 +200,42 @@ var getAndRegisterEventsForCalendar = function(_oa2Client, cal, userID, teamID) 
 			var jobStart = 'dateTime' in thisEvent.start ? thisEvent.start.dateTime : thisEvent.start.date + 'T' + DAY_START_TIME + '.000Z';
 			jobStart = new Date(jobStart);
 
-			// schedule the job
-			var job = schedule.scheduleJob(jobStart,
-				doEventJob.bind(null, thisEvent, userID, teamID) // bind data to callback (see https://github.com/node-schedule/node-schedule#date-based-scheduling)
-			);
-			
-			// stash job for future cancelling
-			eventJobList[cal.id][thisEvent.id] = job;
+			// only schedule the job if it's in the future
+			if (jobStart > new Date()) {
+				var job = schedule.scheduleJob(jobStart,
+					doEventJob.bind(null, thisEvent, userID, teamID) // bind data to callback (see https://github.com/node-schedule/node-schedule#date-based-scheduling)
+				);
+				
+				if (job != null) {
+					// stash job for future cancelling
+					eventJobList[calFBKey][thisEvent.id] = job;
+				}
+			}
 		}
-		console.log('all events in cycle for calendar ' + cal.id + ' registered, calendars with pending jobs:', Object.keys(eventJobList));
+		console.log(Object.keys(eventJobList).length + ' registered cals');
 	});
 }
 
-/*
-	- remove all scheduled jobs for this calendar
-		- loop through eventJobList, remove all events for this cal
-	NB: uses GCal ID, not FB key!
+/**
+*	remove all scheduled jobs for this calendar
 */
-var onCalRemoved = function(calID) {
+var onCalRemoved = function(calFBKey) {
 	// cancel all jobs associated with that calendar
-	if (calID in eventJobList) {
-		for (var i in eventJobList[calID]) {
-			if (eventJobList[calID][i] != null && 'cancel' in eventJobList[calID][i]) {
-				eventJobList[calID][i].cancel();
-				delete eventJobList[calID][i]; // clean up job
+	if (calFBKey in eventJobList) {
+		for (var i in eventJobList[calFBKey]) {
+			if (eventJobList[calFBKey][i] != null && 'cancel' in eventJobList[calFBKey][i]) {
+				eventJobList[calFBKey][i].cancel();
+				delete eventJobList[calFBKey][i]; // clean up job
 			}
 		}
 	}
-
 	// delete calendar from list
-	delete eventJobList[calID];
-	console.log(calID + ' removed, pending jobs', Object.keys(eventJobList));
+	delete eventJobList[calFBKey];
+	console.log(Object.keys(eventJobList).length + ' registered cals');
 }
  
 /*
-	post status update for the event
+*	post status a update for the event
 */
 var doEventJob = function(event, userID, teamID) {
 	console.log('doEventJob', event.summary, userID, teamID);
