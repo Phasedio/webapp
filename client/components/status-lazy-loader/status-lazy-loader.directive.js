479674978fa470e7c8ba0c56779559963a6fc619
Dave Riedstra <dave.riedstra@gmail.com>
2016-04-02 13:17:16 -0700

    style loader, add back to top link

--- client/components/status-lazy-loader/status-lazy-loader.directive.js
+++ client/components/status-lazy-loader/status-lazy-loader.directive.js
@@ -1,11 +1,19 @@
 angular.module('webappApp')
-	.directive('statusLazyLoader', ['Phased', function(Phased) {
+	.directive('statusLazyLoader', ['Phased', '$window', function(Phased, $window) {
 		return {
 			restrict: 'A',
 			replace: true,
 			templateUrl: 'components/status-lazy-loader/status-lazy-loader.template.html',
 			link: function($scope, $el, $attrs) {
-				$scope.testSth = Phased.getStatusesPage;
+				$scope.loadMore = Phased.getStatusesPage;
+
+				// simple scroll-to-top fn
+				// shouldn't need this (should just use href="#"),
+				// but angular is interrupting local anchors
+				$scope.goToTop = function(e) {
+					e.preventDefault();
+					$window.scrollTo(0, 0);
+				}
 			}
 		}
 	}])
