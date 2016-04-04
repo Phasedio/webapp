9bf5036d6ed937ce7fb81f70e0306ded9de53ce6
Brian Best <brian.brianb64@gmail.com>
2016-04-04 18:03:24 -0300

    members are now shown in team page with todo lists

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,15 +4,42 @@ angular.module('webappApp')
   .controller('MainCtrl', function ($scope, Auth, Phased, toaster, $location) {
     ga('send', 'pageview', '/team');
     $scope.team = Phased.team;
+    $scope.taskList = Phased.get.tasks;
     $scope.Phased = Phased;
     $scope.logout = function(){
       console.log('logging you out');
       Auth.logout();
       $location.path('/login');
     }
-    // $scope.getTasks = function(id){
-    //
-    // }
+    $scope.getTasks = {}
+
+    function getUserTasks(){
+      var members = {};
+      _.forEach(Phased.team.members, function(value, key) {
+        console.log(key);
+        var userTasks = [];
+        var user = key;
+        _.forEach(Phased.get.tasks, function(value, key) {
+          if (value.assigned_to == user && userTasks.length < 5) {
+            userTasks.push(key);
+
+          }
+        });
+        members[user] = userTasks;
+      });
+      $scope.getTasks = members;
+      console.log(members);
+      //$scope.$digest();
+    }
+    if (Phased.SET_UP) {
+      getUserTasks();
+    }
+
+    $scope.$on('Phased:setup', function() {
+      getUserTasks();
+      // $scope.$digest(); // instead of apply; only affects current scope instead of rootscope
+    });
+
     // $scope.canAddMembers = function(){
     //   var k = Object.keys(Phased.team.members);
     //   $scope.numMembers = k.length;
