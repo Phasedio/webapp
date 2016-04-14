9e311383bbacc3fc20e58655a9eff7c00f1b3d54
Brian Best <brian.brianb64@gmail.com>
2016-04-14 17:56:15 -0300

    github test

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@
 // process.env.NODE_ENV = process.env.NODE_ENV || 'test'; // for testing sessions, webhooks, & api routes
 process.env.NODE_ENV = 'development';
 //process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+//githubtest
 
 var express = require('express');
 var config = require('./config/environment');
